4719496d0cf4626009eaec3a45bb544fa0b1093d
Sasha Gorbar <sanyagor23@gmail.com>
2021-10-31 22:48:41 +0200

    Добавьте файлы проекта.

---
+++ quoridor/MainForm.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace quoridor
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label labelScore1;
+        private System.Windows.Forms.Label labelRemainTime1;
+        private System.Windows.Forms.Label labelBlocks1;
+        private System.Windows.Forms.Button buttonColor1;
+        private System.Windows.Forms.Label labelScore2;
+        private System.Windows.Forms.Label labelRemainTime2;
+        private System.Windows.Forms.Label labelBlocks2;
+        private System.Windows.Forms.Button buttonColor2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label labelCurrentTime;
+        private System.Windows.Forms.Label labelCurrentPlayer;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button buttonSquareColor;
+        private System.Windows.Forms.Button buttonCanvasColor;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button buttonBlockColor;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ quoridor/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("quoridor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("quoridor")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
